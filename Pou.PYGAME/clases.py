5ab51b52b5b57c2e86b6599bf2f2c195ce88b9c7
iaragonzalezzz <123982656+iaragonzalezzz@users.noreply.github.com>
2023-12-14 12:26:02 -0300

    clases.py

--- Pou.PYGAME/clases.py
+++ Pou.PYGAME/clases.py
@@ -3,6 +3,7 @@ import random
 from constantes import *
 from imagenes import *
 
+
 pygame.mixer.init()
 
 pantalla = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
@@ -10,14 +11,17 @@ pantalla = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
 class Pou(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("Pou.PYGAME./arch.imagenes/img_pou_1.png")
+        self.image = pygame.image.load("arch.imagenes/img_pou_1.png")
         self.rect = self.image.get_rect()
+        # Atributo
         self.rect.topleft = (ANCHO_VENTANA // 2, ALTO_VENTANA - 100)
+        # Atributo
         self.velocidad = 10
+        # Atributo
         self.vidas = 3
 
-    def actualizar(self, keys, numero_imagen):
-        self.image = pygame.image.load(f"Pou.PYGAME./arch.imagenes/img_pou_{numero_imagen}.png")
+    # Metodo
+    def actualizar(self, keys):
         if keys[pygame.K_LEFT]:
             self.rect.x -= self.velocidad
         if keys[pygame.K_RIGHT]:
@@ -26,6 +30,11 @@ class Pou(pygame.sprite.Sprite):
             self.rect.x = 0
         elif self.rect.x > ANCHO_VENTANA - self.rect.width:
             self.rect.x = ANCHO_VENTANA - self.rect.width
+
+    # Metodo
+    def cambiar_imagen(self, numero_imagen):
+        self.image = pygame.image.load(f"arch.imagenes/img_pou_{numero_imagen}.png")
+
 
 class ComidaBuena(pygame.sprite.Sprite):
     def __init__(self, food, velocidad_comida):
@@ -65,9 +74,9 @@ class ComidaMala(pygame.sprite.Sprite):
 
 
 #sonidos
-comida_buena_s = pygame.mixer.Sound("Pou.PYGAME./sonidos/Y2meta.app - Pou eating _ Pou comendo _ Pou comiendo - Pou Play (128 kbps) (1).mp3")
-comida_mala_s = pygame.mixer.Sound("Pou.PYGAME./sonidos/Y2meta.app - Pou Nah (128 kbps).mp3")
-game_over_s = pygame.mixer.Sound("Pou.PYGAME./sonidos/Y2meta.app - Pou game over sound effect (128 kbps).mp3")
+comida_buena_s = pygame.mixer.Sound("sonidos/comida_buena.mp3")
+comida_mala_s = pygame.mixer.Sound("sonidos/comida_mala.mp3")
+game_over_s = pygame.mixer.Sound("sonidos/game_over.mp3")
 
 #sprites
 todos_los_sprites = pygame.sprite.Group()
@@ -75,6 +84,7 @@ pou = Pou()
 comida_buena = ComidaBuena(food, velocidad_comida = 3)
 comida_mala = ComidaMala(bad, velocidad_comida = 3)
 todos_los_sprites.add(pou, comida_buena, comida_mala)
+
 
 
 
